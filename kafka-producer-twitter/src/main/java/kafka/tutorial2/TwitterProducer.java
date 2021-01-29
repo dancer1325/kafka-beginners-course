44fe735911df7c1055aed05d7df83db016b6054b
alfredo toledano <atoledanocarrizosa@deloitte.es>
2021-01-29 22:15:40 +0100

    - To refactor the code

--- kafka-producer-twitter/src/main/java/kafka/tutorial2/TwitterProducer.java
+++ kafka-producer-twitter/src/main/java/kafka/tutorial2/TwitterProducer.java
@@ -1,23 +1,12 @@
 package kafka.tutorial2;
 
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.util.List;
 import java.util.Properties;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
 
-import com.google.common.collect.Lists;
-import com.twitter.hbc.ClientBuilder;
 import com.twitter.hbc.core.Client;
-import com.twitter.hbc.core.Constants;
-import com.twitter.hbc.core.Hosts;
-import com.twitter.hbc.core.HttpHosts;
-import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
-import com.twitter.hbc.core.processor.StringDelimitedProcessor;
-import com.twitter.hbc.httpclient.auth.Authentication;
-import com.twitter.hbc.httpclient.auth.OAuth1;
 import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
@@ -27,37 +16,11 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class TwitterProducer {
+public class TwitterProducer extends TwitterClient {
 
     Logger logger = LoggerFactory.getLogger(TwitterProducer.class.getName());
 
-    // use your own credentials - don't share them with anyone
-    private static Properties properties;
-    private static String consumerKey;
-    private static String consumerSecret;
-    private static String token;
-    private static String secret;
-
-//    List<String> terms = Lists.newArrayList("bitcoin", "usa", "politics", "sport", "soccer");
-    List<String> terms = Lists.newArrayList("bitcoin");
-
-
     public TwitterProducer() throws IOException {
-        //How to indicate the file's path
-        //1º Relative path (It doesn't work, why?)
-        String rootRelativePath = "src/main/resources/";
-        //2º By the thread
-        String rootThreadPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
-
-        String secretPropertiesPath = rootThreadPath + "application-secret.properties";
-        FileInputStream fileInputStream = new FileInputStream(secretPropertiesPath);
-        properties = new Properties();
-        properties.load(fileInputStream);
-
-        consumerKey = properties.getProperty("consumerkey");
-        consumerSecret = properties.getProperty("consumerSecret");
-        token = properties.getProperty("token");
-        secret = properties.getProperty("secret");
     }
 
     public static void main(String[] args) throws IOException {
@@ -116,28 +79,6 @@ public class TwitterProducer {
             }
         }
         logger.info("End of application");
-    }
-
-    public Client createTwitterClient(BlockingQueue<String> msgQueue){
-
-        /** Declare the host you want to connect to, the endpoint, and authentication (basic auth or oauth) */
-        Hosts hosebirdHosts = new HttpHosts(Constants.STREAM_HOST);
-        StatusesFilterEndpoint hosebirdEndpoint = new StatusesFilterEndpoint();
-
-        hosebirdEndpoint.trackTerms(terms);
-
-        // These secrets should be read from a config file
-        Authentication hosebirdAuth = new OAuth1(consumerKey, consumerSecret, token, secret);
-
-        ClientBuilder builder = new ClientBuilder()
-                .name("Hosebird-Client-01")                              // optional: mainly for the logs
-                .hosts(hosebirdHosts)
-                .authentication(hosebirdAuth)
-                .endpoint(hosebirdEndpoint)
-                .processor(new StringDelimitedProcessor(msgQueue));
-
-        Client hosebirdClient = builder.build();
-        return hosebirdClient;
     }
 
     public KafkaProducer<String, String> createKafkaProducer(){
